b73eb2eaa085fcec585283c19cd46f2c295373c3
dengzi <denua@qq.com>
2019-09-11 20:01:02 +0800

    try fix some build problem. but failed. wtf IDEA plugin.

--- src/main/java/com/dengzii/plugin/findview/MainAction.java
+++ src/main/java/com/dengzii/plugin/findview/MainAction.java
@@ -1,54 +1,74 @@
 package com.dengzii.plugin.findview;
 
 import com.dengzii.plugin.findview.utils.XLog;
-import com.intellij.lang.Language;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.application.Result;
-import com.intellij.openapi.command.WriteCommandAction;
-import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.fileEditor.FileEditor;
-import com.intellij.openapi.fileTypes.FileType;
-import com.intellij.psi.*;
-import com.intellij.psi.impl.source.tree.LeafPsiElement;
-import com.intellij.psi.search.FilenameIndex;
-import com.intellij.psi.tree.IElementType;
+import com.intellij.openapi.editor.event.DocumentEvent;
+import com.intellij.openapi.editor.event.DocumentListener;
+import com.intellij.openapi.project.Project;
+//import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.SyntheticElement;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.psi.util.PsiUtilBase;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+
 public class MainAction extends AnAction {
+
+    private static final String TAG = "MainAction";
+
+//    private PsiClass mClass;
+    private Project mProject;
+    private PsiFile mPsiFile;
+
+//    public MainAction() {
+//        super(null);
+//    }
+//
+//    protected MainAction(CodeInsightActionHandler handler) {
+//        super(handler);
+//    }
 
     @Override
     public void actionPerformed(AnActionEvent e) {
 
         Editor editor = e.getData(PlatformDataKeys.EDITOR);
-        PsiFile psiFile = e.getData(PlatformDataKeys.PSI_FILE);
-        FileEditor fileEditor = e.getData(PlatformDataKeys.FILE_EDITOR);
 
+        this.mProject = e.getProject();
+        this.mPsiFile = PsiUtilBase.getPsiFileInEditor(editor, mProject);
+//        this.mClass = getTargetClass(editor, mPsiFile);
 
-        XLog.info("isValid", psiFile.isValid());
-        XLog.info("name", psiFile.getName());
-        XLog.info("type", psiFile.getFileType());
-        XLog.info("language", psiFile.getLanguage());
-        System.out.println(psiFile.getText());
-
-
-        PsiFile file = PsiFileFactory.getInstance(e.getProject()).createFileFromText(psiFile.getLanguage(), "HelloWorld");
-        psiFile.add(file);
-
-        ApplicationManager.getApplication().invokeLater(new Runnable() {
+        Document document = editor.getDocument();
+//        XLog.info(TAG, "className: " + mClass.getName());
+//        XLog.info(TAG, "superClass: " + Arrays.toString(mClass.getSupers()));
+//        XLog.info(TAG, "fields: " + Arrays.toString(mClass.getAllFields()));
+        document.addDocumentListener(new DocumentListener() {
             @Override
-            public void run() {
-                new WriteCommandAction(e.getProject()) {
+            public void beforeDocumentChange(@NotNull DocumentEvent event) {
+                XLog.info(TAG, "beforeDocumentChange: " + event.getOldFragment());
+            }
 
-                    @Override
-                    protected void run(@NotNull Result result) throws Throwable {
-
-                    }
-                };
+            @Override
+            public void documentChanged(@NotNull DocumentEvent event) {
+                XLog.info(TAG, "documentChanged: " + event.getNewFragment());
             }
         });
     }
+
+//    private PsiClass getTargetClass(Editor editor, PsiFile file) {
+//        int offset = editor.getCaretModel().getOffset();
+//        PsiElement element = file.findElementAt(offset);
+//        if (element == null) {
+//            return null;
+//        } else {
+//            PsiClass target = PsiTreeUtil.getParentOfType(element, PsiClass.class);
+//            return target instanceof SyntheticElement ? null : target;
+//        }
+//    }
 }
--- src/main/java/com/dengzii/plugin/findview/actions/InsertFindViewAction.java
+++ src/main/java/com/dengzii/plugin/findview/actions/InsertFindViewAction.java
@@ -1,19 +1,27 @@
 package com.dengzii.plugin.findview.actions;
 
+import com.intellij.openapi.application.Result;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiFile;
+import com.intellij.util.ThrowableRunnable;
+import org.jetbrains.annotations.NotNull;
 
-public class InsertFindViewAction extends WriteCommandAction.Simple {
+public class InsertFindViewAction extends WriteCommandAction {
 
     protected InsertFindViewAction(Project project, PsiFile... files) {
         super(project, files);
+        try {
+            WriteCommandAction.writeCommandAction(project, files).run((ThrowableRunnable<Throwable>) () -> {
 
+            });
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
     }
 
     @Override
-    protected void run() throws Throwable {
-
+    protected void run(@NotNull Result result) throws Throwable {
 
     }
 }
