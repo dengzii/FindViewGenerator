274a27614fd778d4ccb8152998974acc991deb92
dengzi <denua@qq.com>
2019-09-30 10:58:34 +0800

    update: hide Action when file is empty or not support

--- src/com/dengzii/plugin/findview/MainAction.java
+++ src/com/dengzii/plugin/findview/MainAction.java
@@ -3,6 +3,7 @@ package com.dengzii.plugin.findview;
 import com.dengzii.plugin.findview.gen.FindViewCodeWriter;
 import com.dengzii.plugin.findview.ui.ViewIdMappingDialog;
 import com.dengzii.plugin.findview.utils.PsiFileUtils;
+import com.intellij.lang.Language;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
@@ -10,9 +11,9 @@ import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
-import java.util.Objects;
 
 /**
  * <pre>
@@ -32,7 +33,7 @@ public class MainAction extends AnAction {
         PsiFile psiFile = e.getData(CommonDataKeys.PSI_FILE);
         Editor editor = e.getData(CommonDataKeys.EDITOR);
 
-        if (Objects.isNull(project) || Objects.isNull(psiFile) || Objects.isNull(editor)) {
+        if (isNull(project, psiFile, editor)) {
             return;
         }
 
@@ -46,5 +47,35 @@ public class MainAction extends AnAction {
             }
             WriteCommandAction.writeCommandAction(project).run(new FindViewCodeWriter(psiFile, viewInfos));
         }
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+        super.update(e);
+        Project project = e.getProject();
+        PsiFile psiFile = e.getData(CommonDataKeys.PSI_FILE);
+        Editor editor = e.getData(CommonDataKeys.EDITOR);
+
+        e.getPresentation().setEnabledAndVisible(true);
+
+        if (isNull(project, psiFile, editor)) {
+            e.getPresentation().setEnabledAndVisible(false);
+            return;
+        }
+        Language language = psiFile.getLanguage();
+        if (!language.is(Config.JAVA) && !language.is(Config.KOTLIN)) {
+            e.getPresentation().setEnabledAndVisible(false);
+        }
+
+    }
+
+    private boolean isNull(Object... objects) {
+        for (Object o : objects) {
+            if (o == null) {
+                return true;
+            }
+        }
+        return false;
     }
 }
